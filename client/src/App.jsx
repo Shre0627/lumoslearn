57cb3500de64429eef410876a6ed2cd901b5251d
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-06-05 12:20:51 -0400

    initial widget changes

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <BgClock/>
+      <WidgetNav/>
+      <MenuPanel/>
+      <SettingsButton/>
+    </>
+  );
+}
+
+export default App;
